15b6b475083dbdc8f86723d9c8ec3422bed472ca
RyhaveanProject <bfnsscxj97@privaterelay.appleid.com>
2026-05-08 17:55:27 +0400

    Create api.js for backend API integration
    Implement API module with axios for backend communication and auth header management.

---
+++ frontend/src/lib/api.js
@@ -0,0 +1,26 @@
+import axios from \"axios\";
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+export const API = `${BACKEND_URL}/api`;
+
+export const api = axios.create({ baseURL: API });
+
+export function setAuthHeaders({ token, deviceId }) {
+  if (token) api.defaults.headers.common[\"Authorization\"] = `Bearer ${token}`;
+  else delete api.defaults.headers.common[\"Authorization\"];
+  if (deviceId) api.defaults.headers.common[\"X-Device-Id\"] = deviceId;
+}
+
+api.interceptors.response.use(
+  (r) => r,
+  (err) => {
+    const status = err?.response?.status;
+    const detail = err?.response?.data?.detail;
+    if (status === 409 && detail === \"DEVICE_MISMATCH\") {
+      window.dispatchEvent(new CustomEvent(\"ryh:device-mismatch\"));
+    } else if (status === 401) {
+      window.dispatchEvent(new CustomEvent(\"ryh:unauthorized\"));
+    }
+    return Promise.reject(err);
+  }
+);
